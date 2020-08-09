8021ec1d35d2b01d07d151107e004c54846167fa
Ahlam Saleh <ahlamsaleh199700@gmail.com>
2020-08-10 08:54:31 +1000

    Task 1 Complete

--- testSQL.sql
+++ testSQL.sql
@@ -1,3 +1,19 @@
 /* Name - Ahlam Hassen
    StudentID - 103031281 */
 
+--Task 1
+/* 
+Tour (TourName, Description)
+Primary Key (TourName)
+
+Client (ClientID, Surname, GivenName, Gender)
+Primary Key (ClientID)
+
+Event (TourName, EventYear, EventMonth, EventDay, fee)
+Primary Key (TourName, EventYear, EventMonth, EventDay)
+Foreign Key (TourName) References Tour
+
+Booking (ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment)
+Primary Key (ClientID, TourName, EventYear, EventMonth, EventDay)
+Foreign Key (ClientID) References Client
+Foreign Key (TourName, EventYear, EventMonth, EventDay) References Event */ 
